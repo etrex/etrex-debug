c8d1264407eeeff551ac40333d3cc874a969c661
etrex kuo <et284vu065k3@gmail.com>
2023-11-27 03:06:14 +0800

    接受所有路由

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'http_requests#index'
   resources :http_requests, only: [:index, :show, :create]
-  get    'create', to: 'http_requests#create'
-  post   'create', to: 'http_requests#create'
-  patch  'create', to: 'http_requests#create'
-  put    'create', to: 'http_requests#create'
-  delete 'create', to: 'http_requests#create'
+
   get    'delete', to: 'http_requests#delete'
+  get    'create', to: 'http_requests#create'
+
+  # 所有路由
+  match '*path', to: 'http_requests#create', via: :all
 end
